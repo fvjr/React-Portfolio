1291c3f542b54545341b7805ee59e6f576b00a65
Fernando Vasquez <vasquezjrf@gmail.com>
2022-04-22 22:23:14 -0400

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 
+const styles = {
+  textAlign: "center",
+  background: "gray",
+  color: "white",
+  width: "100%",
+  marginTop: "2rem",
+  padding: "2rem",
+  position: "fixed",
+  bottom: "0",
+  left: "0",
+};
+
 export default function Footer() {
   return (
     <div>
-      <p>FOOTER - 2022 Fernando Vasquez - fvjr</p>
+      <p style={styles}>FOOTER - 2022 Fernando Vasquez - fvjr</p>
     </div>
   );
 }
